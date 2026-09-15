86df48cba7f6ddb13729bcb3888566de4d836175
Sjors Robroek <s.robroek@vxsan.com>
2026-09-15 19:14:27 +0400

    fix(browser-tools): normalize resolver imports

--- browser-tools/extensions/lib/config.ts
+++ browser-tools/extensions/lib/config.ts
@@ -1,7 +1,7 @@
-import * as pluginDirs from "@oh-my-pi/pi-utils/dirs";
 import { readFile } from "node:fs/promises";
 import { isAbsolute, normalize } from "node:path";
 import type { PluginSettingSchema } from "@oh-my-pi/pi-coding-agent/extensibility/plugins";
+import * as pluginDirs from "@oh-my-pi/pi-utils/dirs";
 
 type PluginDirsModule = {
 	getPluginsLockfile(home?: string): string;
