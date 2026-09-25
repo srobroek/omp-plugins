20862e8c03f0a758c6da7ed86bf14c6d3703bbb1
Sjors Robroek <s.robroek@vxsan.com>
2026-09-25 22:35:05 +0400

    chore(deps): bump OMP packages to 18.3.1 (#576)

--- browser-tools/dist/headed-browser-tools.js
+++ browser-tools/dist/headed-browser-tools.js
@@ -40247,6 +40247,7 @@ import { isAbsolute as isAbsolute2, normalize as normalize2 } from "path";
 var exports_dirs = {};
 __export(exports_dirs, {
   APP_NAME: () => APP_NAME,
+  APP_URL: () => APP_URL,
   CONFIG_DIR_NAME: () => CONFIG_DIR_NAME,
   MAIN_CONFIG_FILENAMES: () => MAIN_CONFIG_FILENAMES,
   MIN_BUN_VERSION: () => MIN_BUN_VERSION,
@@ -40295,7 +40296,6 @@ __export(exports_dirs, {
   getGithubCacheDbPath: () => getGithubCacheDbPath,
   getGlobalDaemonRuntimeDir: () => getGlobalDaemonRuntimeDir,
   getGlobalDaemonRuntimeRoot: () => getGlobalDaemonRuntimeRoot,
-  getGpuCachePath: () => getGpuCachePath,
   getHistoryDbPath: () => getHistoryDbPath,
   getInstallId: () => getInstallId,
   getLastChangelogVersionPath: () => getLastChangelogVersionPath,
@@ -40340,6 +40340,7 @@ __export(exports_dirs, {
   getWorktreeDir: () => getWorktreeDir,
   getWorktreesDir: () => getWorktreesDir,
   hashPath: () => hashPath,
+  localDay: () => localDay,
   normalizePathForComparison: () => normalizePathForComparison,
   normalizeProfileName: () => normalizeProfileName,
   pathIsWithin: () => pathIsWithin,
@@ -40357,7 +40358,7 @@ import * as fs from "fs";
 import * as os from "os";
 import * as path from "path";
 // node_modules/@oh-my-pi/pi-utils/package.json
-var version = "18.2.4";
+var version = "18.3.1";
 var engines = {
   bun: ">=1.3.14"
 };
@@ -40375,6 +40376,7 @@ function isEnotdir(err) {
 
 // node_modules/@oh-my-pi/pi-utils/src/dirs.ts
 var APP_NAME = "omp";
+var APP_URL = "https://omp.sh/";
 var CONFIG_DIR_NAME = ".omp";
 var MAIN_CONFIG_FILENAMES = ["config.yml", "config.yaml"];
 var VERSION = version;
@@ -40695,8 +40697,11 @@ function getReportsDir() {
 function getLogsDir() {
   return dirs.rootSubdir("logs", "state");
 }
+function localDay(date) {
+  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}`;
+}
 function getLogPath(date = new Date, pid = process.pid) {
-  return path.join(getLogsDir(), `${APP_NAME}.${date.toISOString().slice(0, 10)}.${pid}.log`);
+  return path.join(getLogsDir(), `${APP_NAME}.${localDay(date)}.${pid}.log`);
 }
 function getPluginsDir(home) {
   if (home !== undefined && home !== RESOLVER_HOME) {
@@ -40767,9 +40772,6 @@ function hashPath(absPath) {
 }
 function getWorktreeDir(segment) {
   return path.join(getWorktreesDir(), segment);
-}
-function getGpuCachePath() {
-  return dirs.rootSubdir("gpu_cache.json", "cache");
 }
 function getGithubCacheDbPath() {
   const override = process.env.OMP_GITHUB_CACHE_DB;
